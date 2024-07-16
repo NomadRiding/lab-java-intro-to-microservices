5f8291a882074ae1eb0265052ad069738a6265de
nomadriding <fundora.a@gmail.com>
2024-07-16 15:32:24 -0400

    adding files that didn't load

--- student-info-service/src/main/java/com/ironhack/studentinfoservice/controller/StudentController.java
+++ student-info-service/src/main/java/com/ironhack/studentinfoservice/controller/StudentController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api")
 public class StudentController {
